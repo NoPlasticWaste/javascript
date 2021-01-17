d351c55f7b05d78b09b79b3c9c7f2e4a68ffb869
Endarlani <endarlan11@gmail.com>
2021-01-17 10:04:42 +0700

    tugas 6

---
+++ tugas6.js
@@ -0,0 +1,11 @@
+// Buatlah sebuah perulangan dengan kondisi <=20 atau sebanyak 20 kali
+// Setelah itu teman teman buatlah sebuah kondisi pada perulangan jika dia bilangan genap dia akan menampilkan text bilangan genap bukan angkanya, tetapi kalau yang tampil bilangan ganjil , biarkan angka nya yang ditampilkan.
+// Tampilkan hasilnya dengan console.log()
+
+for(var i=1; i<=20; i++){
+    if(i%2==0){
+        console.log("Bilangan Genap")
+    }else{
+        console.log(i)
+    }
+}
