e682cedfa1d8bc0c59b83d11b1e2cd21db35a392
Endarlani <endarlan11@gmail.com>
2021-01-17 09:00:16 +0700

    tugas 4 revisi

--- tugas4.js
+++ tugas4.js
@@ -7,16 +7,18 @@ var b = 70;
 var c = 90;
 
 function sort (a, b, c) {
-    if(a > b && b > c) {
+    if(a > b && b > c && a > c) {
       console.log(a, b , c);
-    } else if (a < b && a > c) {
+    } else if (a > b && b < c) {
+        console.log(a, c, b);
+    } else if (a < b && b > c && a > c) {
         console.log(b, a, c);
-    } else if (b > a && a < c) {
-        console.log(b, c, a);
-    } else if(b < a && c > b) {
-        console.log(a, c, b); 
-    } else if(c > a && a > b) {
+    } else if(a < b && b > c && a < c) {
+        console.log(b, c, a); 
+    } else if(a < b && b > c && a > c) {
         console.log(c, a, b);
+    } else if(a < b && b < c) {
+        console.log(c, b, a);
     } else {
         console.log(c, b , a);
     }
