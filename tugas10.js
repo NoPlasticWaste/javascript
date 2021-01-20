97836017631200fda15042e3b85e12b12b7dee39
Endarlani <endarlan11@gmail.com>
2021-01-20 14:17:37 +0700

    tugas 9 revisi

--- tugas10.js
+++ tugas10.js
@@ -1,5 +1,6 @@
 function sumTriangularNums() {
-    let sum = 0;  for (let i = 0; i <= 55; i++) {
+    var sum = 0;
+    for (var i = 0; sum <= 50; i++) {
       sum += (i + 1);
       console.log(sum)
     }
