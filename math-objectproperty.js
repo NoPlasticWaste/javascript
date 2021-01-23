39d14f2626345b9e14ea9587fdcff10af63369d3
Endarlani <endarlan11@gmail.com>
2021-01-23 21:39:50 +0700

    materi sebelum tugas 14

---
+++ math-objectproperty.js
@@ -0,0 +1,8 @@
+console.log(Math.E) //angka log natural e
+console.log(Math.LN10) //angka log natural 10
+console.log(Math.LN2) //angka log natural 2
+console.log(Math.LOG10E) //angka log natural e basis 10
+console.log(Math.LOG2E) //angka log natural e basis 2
+console.log(Math.PI) //angka pi
+console.log(Math.SQRT1_2) //angka 1 dibagi akar kuadrat 2
+console.log(Math.SQRT2) //angka akar kuadrat 2
