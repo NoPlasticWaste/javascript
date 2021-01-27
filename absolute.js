2d65818054ce5eef5e01b060a21bd716b994668f
NoPlasticWaste <moza.mzrl@gmail.com>
2021-01-27 11:29:04 +0800

    tugas 15

---
+++ absolute.js
@@ -0,0 +1,5 @@
+var x = 30;
+var y = -20;
+
+console.log(y-x)
+console.log(Math.abs(y-x))
---
+++ min-max.js
@@ -0,0 +1,9 @@
+function MinMax(){
+    var min = Math.min(67, 98, 614, 98);
+    console.log(min);
+
+    var max = Math.max(76,451,978,6);
+    console.log(max)
+}
+
+MinMax()
