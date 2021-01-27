cdff1a51a5f260aeab578d9d8e57e613f6f85ee7
NoPlasticWaste <moza.mzrl@gmail.com>
2021-01-27 11:01:14 +0800

    tugas 14

---
+++ tugas14.js
@@ -0,0 +1,11 @@
+// Teman teman buatlah sebuah variabel untuk melakukan operasi pembagian , teman teman lakukan pembagian 37 dibagi tanggal lahir teman teman.
+// Setelah itu teman teman tampilkan hasil pembagian tersebut.
+// Setelah itu teman teman tampilkan kembali hasilnya dengan perintah math round , ceil dan floor.
+
+tanggal = 11
+bagi = 37/(tanggal)
+
+console.log(bagi)
+console.log(Math.round(bagi))
+console.log(Math.ceil(bagi))
+console.log(Math.floor(bagi))
