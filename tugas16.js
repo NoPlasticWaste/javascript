b193b5e33317c38f18642adb1c4ccdbc8432b4c0
NoPlasticWaste <moza.mzrl@gmail.com>
2021-01-27 11:33:12 +0800

    tugas 16

---
+++ tugas16.js
@@ -0,0 +1,8 @@
+// Carilah nilai minimal dari deret angka : 45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10
+// Carilah nilai maksimal dari deret angka : 45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5
+
+x = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+y = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+
+console.log("Nilai minimal :", x)
+console.log("Nilai maksimal :", y)
